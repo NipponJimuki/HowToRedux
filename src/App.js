71be49532ea0da12e8e7f998584b04dd81a9b4ba
NJC4746 <kaida.ryo.ichi@njc.co.jp>
2018-11-21 01:46:04 +0900

    initialCommit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const store = createStore(() => {});
+
+// DOM出力
+render(
+    <Provider store={store}>
+        <div>Text</div>
+    </Provider>,
+    document.getElementById('content')
+);
